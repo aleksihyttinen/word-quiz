2ff5eef343126d1769d5a5fd448506b5e25c2f09
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-03 10:54:20 +0200

    Add state hook for word language

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 function App() {
   const [word, setWords] = useState([]);
+  const [language, setLanguage] = useState("english");
   return <div className="App"></div>;
 }
 

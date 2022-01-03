d2fd8d4e161283a5e604210ccdb8ceff18a93c46
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-03 10:55:30 +0200

    Add useEffect for fetching words

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import { useState, useEffect } from "react";
+const axios = require("axios").default;
 function App() {
-  const [word, setWords] = useState([]);
+  const [words, setWords] = useState([]);
   const [language, setLanguage] = useState("english");
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3010/${language}`)
+      .then((response) => setWords(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [language]);
   return <div className="App"></div>;
 }
 

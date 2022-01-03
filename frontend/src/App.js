e72276e2a34b5c2603d7a5a36766092af006eb57
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-03 11:14:55 +0200

    Add second list of words

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,43 @@
 import { useState, useEffect } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 const axios = require("axios").default;
 function App() {
-  const [words, setWords] = useState([]);
-  const [language, setLanguage] = useState("english");
+  const [words1, setWords1] = useState([]);
+  const [words2, setWords2] = useState([]);
+  const [languages, setLanguages] = useState({
+    first: "english",
+    second: "finnish",
+  });
   useEffect(() => {
     axios
-      .get(`http://localhost:8080/${language}`)
-      .then((response) => setWords(response.data))
+      .get(`http://localhost:8080/${languages.first}`)
+      .then((response) => setWords1(response.data))
       .catch((error) => {
         console.log(error);
       });
-  }, [language]);
+  }, [languages.first]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/${languages.second}`)
+      .then((response) => setWords2(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [languages.second]);
   return (
     <div className="App">
-      {words.map((word) => (
-        <p>{word.word}</p>
-      ))}
+      {languages.first}
+      <ListGroup>
+        {words1.map((word) => (
+          <ListGroup.Item key={word.id}>{word.word}</ListGroup.Item>
+        ))}
+      </ListGroup>
+      {languages.second}
+      <ListGroup>
+        {words2.map((word) => (
+          <ListGroup.Item key={word.id}>{word.word}</ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 }

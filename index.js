15b3d0ce17b6ead57ca225cce35afc50e4957c0a
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-03 10:36:35 +0200

    Add get with language parameter

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("frontend/build"));
+
+app.get("/:language", async (req, res) => {
+  let language = req.params.language;
+  try {
+    let data = await connection.getAll(language);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.statusCode = 400;
+    res.end();
+  }
+});
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {

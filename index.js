aedd42614c8b85627aa4bc24b22be95e39ccc24f
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-03 10:03:05 +0200

    Add server listener

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 app.use(express.static("frontend/build"));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});

8293638fb1c5bb7709407eff01df2a8bd7fd44bc
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-04 11:40:34 +0200

    Add function changeLanguage

--- frontend/src/LanguageSelection.js
+++ frontend/src/LanguageSelection.js
@@ -1,6 +1,23 @@
 export default function LanguageSelection(props) {
+  const changeLanguage = (e) => {
+    if (props.whichList === "first") {
+      props.setDisplayedLanguages({
+        first: e.target.value,
+        second: props.displayedLanguages.second,
+      });
+    } else {
+      props.setDisplayedLanguages({
+        first: props.displayedLanguages.first,
+        second: e.target.value,
+      });
+    }
+  };
   return (
-    <select defaultValue={props.selected} name="languages">
+    <select
+      defaultValue={props.selected}
+      onChange={changeLanguage}
+      name="languages"
+    >
       {props.languages.map((language) => {
         return (
           <option key={language} value={language}>

ee107764155cf1b612bd422ac8beae2eefdc5bf9
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-04 14:46:55 +0200

    Add password requirement for teacherview

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 function App() {
+  const navigate = useNavigate();
+  const teacherPassword = "password";
+  const authenticate = () => {
+    let password = prompt("Enter password");
+    if (password === teacherPassword) {
+      navigate("/teacher");
+    } else if (password !== null) {
+      console.log(password);
+      alert("Wrong password!");
+    }
+  };
   return (
     <div className="app">
       <h1>Welcome to the word quiz app. Please select your role:</h1>
       <Button href="/student">Student</Button>
-      <Button href="/teacher">Teacher</Button>
+      <Button onClick={authenticate}>Teacher</Button>
     </div>
   );
 }

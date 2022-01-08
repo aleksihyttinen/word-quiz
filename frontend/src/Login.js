f4a1d9c604025c45a3151f8858487a9d0db0fb58
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-08 18:49:02 +0200

    Fix handleLogin and form functionality

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,22 +1,31 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "./useAuth.js";
 import "./App.css";
+import { useAuth } from "./useAuth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
   const navigate = useNavigate();
-  const { login } = useAuth();
-
+  const password = "password";
+  const [input, setInput] = useState("");
+  const { signin } = useAuth();
   const handleLogin = () => {
-    login().then(() => {
+    if (input !== password) {
+      alert("Wrong password");
+    } else {
+      signin();
       navigate("/teacher");
-    });
+    }
   };
   return (
     <div className="login">
       <Form className="password-form" onSubmit={handleLogin}>
         <Form.Label>Enter password:</Form.Label>
-        <Form.Control type="password" />
+        <Form.Control
+          type="password"
+          onChange={(e) => setInput(e.target.value)}
+        />
         <Button variant="primary" type="submit">
           Submit
         </Button>

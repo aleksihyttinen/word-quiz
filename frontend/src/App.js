38c0ae8291246ed7cdf56fb7db33c594d5e367a5
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-08 17:02:48 +0200

    Add routing for Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,11 @@ import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 function App() {
   const navigate = useNavigate();
-  const teacherPassword = "password";
-  const authenticate = () => {
-    let password = prompt("Enter password");
-    if (password === teacherPassword) {
-      navigate("/teacher");
-    } else if (password !== null) {
-      console.log(password);
-      alert("Wrong password!");
-    }
-  };
   return (
     <div className="app">
       <h1>Welcome to the word quiz app. Please select your role:</h1>
       <Button href="/student">Student</Button>
-      <Button onClick={authenticate}>Teacher</Button>
+      <Button onClick={() => navigate("/login")}>Teacher</Button>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentView from "./StudentView.js";
 import TeacherView from "./TeacherView.js";
+import Login from "./Login.js";
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}></Route>
       <Route path="/student" element={<StudentView />}></Route>
       <Route path="/teacher" element={<TeacherView />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")

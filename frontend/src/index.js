154301a1fcbe07d594bde2ad7ad547259c2fc06e
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-08 18:46:11 +0200

    Add function component RequireAuth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import StudentView from "./StudentView.js";
 import TeacherView from "./TeacherView.js";
 import Login from "./Login.js";
+import { ProvideAuth } from "./useAuth";
+import { useAuth } from "./useAuth";
+function RequireAuth({ children }) {
+  const { auth } = useAuth();
+  return auth ? children : <Navigate to="/login" replace />;
+}
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-      <Route path="/student" element={<StudentView />}></Route>
-      <Route path="/teacher" element={<TeacherView />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-    </Routes>
-  </BrowserRouter>,
+  <ProvideAuth>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/student" element={<StudentView />}></Route>
+        <Route
+          path="/teacher"
+          element={
+            <RequireAuth>
+              <TeacherView />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </ProvideAuth>,
   document.getElementById("root")
 );

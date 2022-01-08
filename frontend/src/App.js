f2039ee7e055ceeacb8eb42f3b45428383eda4f2
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-08 18:46:43 +0200

    Change teacher button to a link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
-import { useNavigate } from "react-router-dom";
+
 function App() {
-  const navigate = useNavigate();
   return (
     <div className="app">
       <h1>Welcome to the word quiz app. Please select your role:</h1>
       <Button href="/student">Student</Button>
-      <Button onClick={() => navigate("/login")}>Teacher</Button>
+      <Button href="/teacher">Teacher</Button>
     </div>
   );
 }

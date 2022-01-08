4b97615689590b0a3a6f0b33ef1522e61fc31db9
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-08 17:09:51 +0200

    Add function handleLogin

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,20 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./useAuth.js";
 import "./App.css";
 export default function Login() {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleLogin = () => {
+    login().then(() => {
+      navigate("/teacher");
+    });
+  };
   return (
     <div className="login">
-      <Form className="password-form">
+      <Form className="password-form" onSubmit={handleLogin}>
         <Form.Label>Enter password:</Form.Label>
         <Form.Control type="password" />
         <Button variant="primary" type="submit">

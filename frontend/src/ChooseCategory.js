885020988873cb5811bacb9bf5456c521b56e805
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-13 23:25:51 +0200

    Add two different views for student and teacher

--- frontend/src/ChooseCategory.js
+++ frontend/src/ChooseCategory.js
@@ -2,12 +2,12 @@ import Button from "react-bootstrap/Button";
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 const axios = require("axios").default;
-export default function ChooseCategory() {
+export default function ChooseCategory(props) {
   const [categories, setCategories] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
   useEffect(() => {
     axios
-      .get(`/api/`)
+      .get(`http://localhost:8080/api/`)
       .then((response) => {
         setCategories(response.data);
       })
@@ -15,18 +15,26 @@ export default function ChooseCategory() {
         console.log(error);
       });
   }, []);
-  return categories.length !== 0 ? (
-    <div className="choose-category">
-      {categories.map((category) => (
-        <Button
-          key={category.table_name}
-          onClick={() => setSearchParams({ category: category.table_name })}
-        >
-          {category.table_name}
-        </Button>
-      ))}
-    </div>
-  ) : (
-    <div />
-  );
+  if (props.view === "teacher") {
+    return categories.map((category) => (
+      <option key={category.table_name} value={category.table_name}>
+        {category.table_name}
+      </option>
+    ));
+  } else {
+    return categories.length !== 0 ? (
+      <div className="choose-category">
+        {categories.map((category) => (
+          <Button
+            key={category.table_name}
+            onClick={() => setSearchParams({ category: category.table_name })}
+          >
+            {category.table_name}
+          </Button>
+        ))}
+      </div>
+    ) : (
+      <div />
+    );
+  }
 }

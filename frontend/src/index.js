c22f2afb681a0d66b62b2f0df0be7da34a8a8d00
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-14 01:30:13 +0200

    Revert route change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,10 +18,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}></Route>
-        <Route
-          path="/student?category=animals"
-          element={<StudentView />}
-        ></Route>
+        <Route path="/student" element={<StudentView />}></Route>
         <Route
           path="/teacher"
           element={

dfe393b0df0f8911927a8b972003db77c226973c
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-14 13:59:43 +0200

    Commented code

--- frontend/src/useAuth.js
+++ frontend/src/useAuth.js
@@ -1,6 +1,10 @@
 import React, { useContext, createContext } from "react";
-const authContext = createContext();
 
+//This useAuth is a modified verion of multiple different useAuth hooks found online
+//It creates a context which includes functions, signin, signout and isAuth
+//Authentication is stored in localStorage, so it doesn't expire.
+
+const authContext = createContext();
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;

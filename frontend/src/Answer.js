d42673f546d33d13162fb2a5a6758c0ea35f9ce1
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-14 12:52:42 +0200

    Commented code

--- frontend/src/Answer.js
+++ frontend/src/Answer.js
@@ -1,8 +1,11 @@
 export default function Answer(props) {
+  //Returns a input field where a user can input their translation of a word.
   const handleChange = (e) => {
+    //Sets input value to userWords array
     props.userWords[e.target.id] = e.target.value;
   };
   if (props.hasAnswered) {
+    //Disable input if user has submitted answers
     return (
       <input
         onChange={handleChange}
@@ -16,6 +19,7 @@ export default function Answer(props) {
       />
     );
   } else {
+    //Return input field which gets it's coloring based on answer status
     return (
       <input
         onChange={handleChange}

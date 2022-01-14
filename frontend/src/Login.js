b3ca55b872367586a5ea3a679a5c7cb94dcc4014
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-14 13:29:39 +0200

    Commented code

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -6,20 +6,24 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const password = "password";
-  const [input, setInput] = useState("");
-  const { signin } = useAuth();
+  //Returns a simple login page with a input bar
+  const navigate = useNavigate(); // https://reactrouter.com/docs/en/v6/api#usenavigate
+  const password = "password"; //Holds password for authentication, should be changed for actual use
+  const [input, setInput] = useState(""); //Stores input
+  const { signin } = useAuth(); //Get function signin from useAuth
+
   const handleLogin = (e) => {
     e.preventDefault();
     if (input !== password) {
       alert("Wrong password");
     } else {
+      //If password is correct, signin to auth and navigate the url to teacher view
       signin();
       navigate("/teacher");
     }
   };
   return (
+    //On enter press or button click, attempt to login
     <div className="login">
       <Form className="password-form" onSubmit={handleLogin}>
         <Form.Label>Enter password:</Form.Label>

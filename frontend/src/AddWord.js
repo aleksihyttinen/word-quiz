10532a857dfa2fae40547bc4cc62248bff0b851e
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-14 12:50:21 +0200

    Improve variable names and commented code

--- frontend/src/AddWord.js
+++ frontend/src/AddWord.js
@@ -1,22 +1,27 @@
 import { Modal, Button, Form } from "react-bootstrap";
 import { useState } from "react";
 const axios = require("axios").default;
+
 export default function AddWord(props) {
-  const [showModal, setShow] = useState(false);
-  let word = [];
+  //Returns a modal which asks user for a word in three languages
+  const [showModal, setShow] = useState(false); //Stores a boolean that tells if modal should be visible or not
+  let wordArray = []; //Init array that will contain a words translations
   const saveWord = () => {
+    //Removes whitespace from words translations
+    //and then sends the word array to the backend
     let trimmed = false;
-    if (word.length !== 3) {
+    if (wordArray.length !== 3) {
+      //Block post request if word doesn't have all three translations
       alert("Insert word in all three langauges");
       return;
     }
-    for (let i = 0; i < word.length; i++) {
-      word[i] = word[i].trimStart().trimEnd();
+    for (let i = 0; i < wordArray.length; i++) {
+      wordArray[i] = wordArray[i].trimStart().trimEnd();
       trimmed = true;
     }
     if (trimmed) {
       axios
-        .post(`http://localhost:8080/api/${props.category}`, word)
+        .post(`http://localhost:8080/api/${props.category}`, wordArray)
         .then((response) => {
           props.setEdited(true);
           console.log(response);
@@ -39,19 +44,22 @@ export default function AddWord(props) {
             <div style={{ fontWeight: "bold" }}>
               <Form.Label>English:</Form.Label>
               <Form.Control
-                onChange={(e) => (word[0] = e.target.value)}
+                //Set input value to wordArray
+                onChange={(e) => (wordArray[0] = e.target.value)}
                 style={{ marginBottom: 10 }}
                 placeholder="English"
               />
               <Form.Label>Finnish:</Form.Label>
               <Form.Control
-                onChange={(e) => (word[1] = e.target.value)}
+                //Set input value to wordArray
+                onChange={(e) => (wordArray[1] = e.target.value)}
                 style={{ marginBottom: 10 }}
                 placeholder="Finnish"
               />
               <Form.Label>Swedish:</Form.Label>
               <Form.Control
-                onChange={(e) => (word[2] = e.target.value)}
+                //Set input value to wordArray
+                onChange={(e) => (wordArray[2] = e.target.value)}
                 style={{ marginBottom: 10 }}
                 placeholder="Swedish"
               />

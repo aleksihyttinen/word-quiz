d920661799c6c42f8cb246926da8ca1be393d7b9
Aleksi Hyttinen <aleksi.hyttinen@tuni.fi>
2022-01-14 12:55:04 +0200

    Commented code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Button from "react-bootstrap/Button";
 
 function App() {
+  //Returns main page for the app
+  //From main page user can navigate to student or teacher view
   return (
     <div className="app">
       <h1>Welcome to the word quiz app.</h1>
